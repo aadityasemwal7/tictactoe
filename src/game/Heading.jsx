0f37c1e61b26d3bd611b1f4d22ecad94cf78212e
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-12-22 11:26:45 +0530

    initial commit

---
+++ src/game/Heading.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+import React from "react"
+
+export default function Heading(){
+    return(
+        <div>
+            <h1 id="heading">Tic-Tac-Toe</h1>
+        </div>
+    )
+}
